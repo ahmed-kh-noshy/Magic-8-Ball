882bc2fcccc05e84d4d2285027489ac81bf567a8
ahmed khaled <ahmedkhaled5562.ak@gmail.com>
2023-08-01 15:00:37 +0300

    adding random function to display the image

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let ballsArray = [UIImage(named: "ball1"), UIImage(named: "ball2"), UIImage(named: "ball3"), UIImage(named: "ball4"), UIImage(named: "ball5")]
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func askButton(_ sender: UIButton) {
+        imageView.image = ballsArray[Int.random(in: 1 ..< 5)]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
